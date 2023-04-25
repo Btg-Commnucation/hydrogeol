46cbb3023b16e4bb48c8af40207feb21a895246f
Tuvedunyel <mightygodx@gmail.com>
2023-04-25 17:08:27 +0200

    env

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,6 +12,23 @@ const Header = () => {
     const [isLoading, setIsLoading] = useState(true)
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
+    const getMenu = new Promise((resolve, reject) =>{
+        try {
+            const response: Response[] = ky(
+                "better-rest-endpoints/v1/menus/menu-principal",
+                {
+                    prefixUrl: import.meta.env.VITE_FETCH_URL
+                }
+            ).json()
+        } catch (error) {
+            reject(error)
+        }
+    } )
+
+    useEffect(() => {
+        console.log(getMenu)
+    })
+
   return (
     <header>
       <a href="#">
